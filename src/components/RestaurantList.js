cecacb5550e11d8596974aed3bc5ad6b31b2bb60
minitapir <siam-brett@hotmail.fr>
2020-07-27 16:36:43 +0200

    Unit testing : RestaurantList loads restaurants on first render and displays the restaurants

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-export const RestaurantList = () => <div>RestaurantList</div>;
+export const RestaurantList = ({loadRestaurants, restaurants}) => {
+
+    useEffect(() => {
+        loadRestaurants();
+    },[loadRestaurants]);
+
+    return (
+        <ul>
+            { restaurants.map(restaurant => (
+                <li key={restaurant.id}>{ restaurant.name} </li>
+            ))}
+        </ul>
+    )
+}
 
 export default RestaurantList;
